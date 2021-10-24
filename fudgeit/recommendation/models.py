4e70a3275a64c7494c6337b5b4aea1ff6cf8d784
Fahim Tran <46301587+fahimtran@users.noreply.github.com>
2021-10-24 03:12:19 -0400

    Added backend models for Restaurants and Food Items - made sure images can be stored and accessed (#3)
    Working locally on fahimtran's machine

--- fudgeit/recommendation/models.py
+++ fudgeit/recommendation/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+
 # Create your models here.
+class Restaurant(models.Model):
+    image = models.ImageField('image', upload_to='restaurants')
+    name = models.CharField('name', max_length=50)
+    description = models.CharField('description', max_length=200)
+    price_rating = models.PositiveSmallIntegerField('price rating')
+    user_rating = models.DecimalField('user rating', max_digits=3, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class FoodItem(models.Model):
+    FOOD_CLASS_CHOICES = [
+        ('1', 'Entree'),
+        ('2', 'Side'),
+        ('3', 'Drink'),
+        ('4', 'Dessert'),
+        ('5', 'Misc.'),
+    ]
+
+    image = models.ImageField('image', upload_to='food-items')
+    name = models.CharField('name', max_length=50)
+    description = models.CharField('description', max_length=200)
+    food_class = models.CharField('food class', max_length=1, choices=FOOD_CLASS_CHOICES, default='5')
+    restaurant = models.ForeignKey('restaurant', Restaurant)
+
+    def __str__(self):
+        return self.name
+
