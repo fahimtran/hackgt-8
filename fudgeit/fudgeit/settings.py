5619abd2798e4f7fac0c989597eaef83d707f271
Fahim Tran <46301587+fahimtran@users.noreply.github.com>
2021-10-24 03:10:37 -0400

    Added backend models for Restaurants and Food Items - made sure images can be stored and accessed

--- fudgeit/fudgeit/settings.py
+++ fudgeit/fudgeit/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
@@ -125,6 +125,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- fudgeit/recommendation/admin.py
+++ fudgeit/recommendation/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from recommendation.models import Restaurant, FoodItem
 
 # Register your models here.
+admin.site.register(Restaurant)
+admin.site.register(FoodItem)
